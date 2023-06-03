64bae56833ca1f1351145480280b7d68e5d0da65
hassan510-cmd <hassanmahmoud607@gmail.com>
2023-06-03 09:01:12 +0300

    BUILD: add hide_pos_cost_margin, pos_analytic modules

--- pos_analytic_by_config/models/__init__.py
+++ pos_analytic_by_config/models/__init__.py
@@ -1,4 +1,11 @@
+from . import account_move
 from . import account_move_line
+from . import account_payment
+
+from . import stock_move
+from . import stock_picking
+
 from . import pos_config
 from . import pos_order
 from . import pos_session
+from . import pos_payment
